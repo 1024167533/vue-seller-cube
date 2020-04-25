61c1c804284860ae1a35e2b1a0dff0da097de6c8
ally1325 <1024167533@qq.com>
2020-04-25 22:51:21 +0800

    v-header

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+   <v-header :seller = "seller"></v-header>
+  </div>
+</template>
+
+<script>
+import VHeader from 'components/v-header/v-header.vue'
+import { getSeller } from 'api/index.js'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      seller: {}
+    }
+  },
+  created() {
+    this._getSeller()
+  },
+  methods: {
+    _getSeller() {
+      getSeller().then((seller) => {
+        this.seller = seller
+      })
+    }
+  },
+  components: {
+    VHeader
+  }
+}
+</script>
+
+<style lang="stylus">
+#app
+  font-family Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color #2c3e50
+</style>
